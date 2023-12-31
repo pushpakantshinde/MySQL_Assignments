cc742bd5cff91ada73a8ff57892d54f8986330f2
Pushpakant Shinde <119100001+pushpakantshinde@users.noreply.github.com>
2023-12-31 10:36:18 +0530

    assignment 1 updated

--- sql assignments.sql
+++ sql assignments.sql
@@ -1,3 +1,5 @@
+# Question 1 - Write SQL query to list the candidates who posses all the required skillss i.e. DS, Tableau, Python, SQL
+
 create database linkedin;
 use linkedin;
 
@@ -18,10 +20,11 @@ insert into skills values
 (3, 'Python'),
 (3, 'SQL');
 
-drop table skills;
 describe skills;
 
 select * from skills;
+
+# Solution 1
 
 select id
 from skills
@@ -29,19 +32,59 @@ where technology in ('DS', 'Tableau', 'SQL', 'Python')
 group by id
 having COUNT(distinct technology) = 4;
 
+# Solution 2
 
 select id
 from skills
 where technology = 'DS'
-and id in
-	(select id
+and id in (
+	select id
 	from skills
-	where technology = 'Tableau')
-	and id in
-		(select id
+	where technology = 'Tableau'
+	and id in (
+		select id
 		from skills
-		where technology = 'SQL')
-		and id in
-			(select id
+		where technology = 'SQL'
+		and id in (
+			select id
 			from skills
-			where technology = 'Python');
+			where technology = 'Python')
+            )
+		);
+ 
+# Question 2 - Write SQL query to return the IDs of the product info that has 0 likes.
+ 
+create database facebook;
+ 
+use facebook;
+ 
+create table product_info(
+product_id int primary key,
+product_name varchar(30)
+);
+ 
+drop table product_info;
+ 
+create table product_info_likes(
+user_id int not null,
+product_id int,
+liked_date date,
+foreign key (product_id) references product_info(product_id)
+);
+ 
+insert into product_info values (1001, 'Blog'), (1002, 'Youtube'), (1003, 'Education');
+ 
+insert into product_info_likes values (1, 1001, '2023-08-19'), (2, 1003, '2023-08-18');
+ 
+select * from product_info;
+select * from product_info_likes;
+
+
+# Solution
+
+select p.product_id
+from product_info p
+left join product_info_likes pl on p.product_id = pl.product_id
+group by p.product_id
+having count(pl.product_id) = 0;
+
